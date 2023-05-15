29b41b7adbb62f7370eacd7c1dad436353aa128f
jackytian <tianbook@126.com>
2023-05-15 16:28:55 +0800

    value expression

---
+++ teaql/src/main/java/io/teaql/data/value/ValueExpression.java
@@ -0,0 +1,20 @@
+package io.teaql.data.value;
+
+public class ValueExpression<T> implements Expression<T, T> {
+
+  private final T value;
+
+  public ValueExpression(T value) {
+    this.value = value;
+  }
+
+  @Override
+  public T eval(T pT) {
+    return pT;
+  }
+
+  @Override
+  public T $getRoot() {
+    return value;
+  }
+}
