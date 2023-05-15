1206b3679b83c5f4f8b9815106a8e5b318e561ce
jackytian <tianbook@126.com>
2023-05-15 15:16:56 +0800

    add expression adaptor

--- teaql/src/main/java/io/teaql/data/value/ExpressionAdaptor.java
+++ teaql/src/main/java/io/teaql/data/value/ExpressionAdaptor.java
@@ -2,71 +2,26 @@ package io.teaql.data.value;
 
 import java.util.function.Function;
 
-public class ExpressionAdaptor<T, E> implements Expression<T, E> {
-
+public class ExpressionAdaptor<T, E, U> implements Expression<T, U> {
   private Expression<T, E> expression;
+  private Function<E, U> function;
 
-  private ExpressionAdaptor(Expression<T, E> expression) {
-    this.expression = expression;
+  public ExpressionAdaptor(Expression<T, E> pExpression, Function<E, U> pFunction) {
+    expression = pExpression;
+    function = pFunction;
   }
 
   @Override
-  public E eval(T pT) {
-    return expression.eval(pT);
-  }
-
-  @Override
-  public <U> Expression<T, U> apply(Function<E, U> function) {
-    return expression.apply(function);
+  public U eval(T pT) {
+    E eval = expression.eval(pT);
+    if (eval == null) {
+      return null;
+    }
+    return function.apply(eval);
   }
 
   @Override
   public T $getRoot() {
     return expression.$getRoot();
-  }
-
-  @Override
-  public E eval() {
-    return expression.eval();
-  }
-
-  @Override
-  public boolean isNull() {
-    return expression.isNull();
-  }
-
-  @Override
-  public boolean isNotNull() {
-    return expression.isNotNull();
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return expression.isEmpty();
-  }
-
-  @Override
-  public boolean isNotEmpty() {
-    return expression.isNotEmpty();
-  }
-
-  @Override
-  public void whenIsNull(Runnable function) {
-    expression.whenIsNull(function);
-  }
-
-  @Override
-  public void whenIsNotNull(Runnable function) {
-    expression.whenIsNotNull(function);
-  }
-
-  @Override
-  public void whenIsEmpty(Runnable function) {
-    expression.whenIsEmpty(function);
-  }
-
-  @Override
-  public void whenNotEmpty(Runnable function) {
-    expression.whenNotEmpty(function);
   }
 }
