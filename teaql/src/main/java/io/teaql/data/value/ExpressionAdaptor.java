93aa6932ee02cf0e1220eb7f64acd02ad72ce034
jackytian <tianbook@126.com>
2023-05-15 15:24:02 +0800

    add expression adaptor

--- teaql/src/main/java/io/teaql/data/value/ExpressionAdaptor.java
+++ teaql/src/main/java/io/teaql/data/value/ExpressionAdaptor.java
@@ -3,25 +3,34 @@ package io.teaql.data.value;
 import java.util.function.Function;
 
 public class ExpressionAdaptor<T, E, U> implements Expression<T, U> {
-  private Expression<T, E> expression;
-  private Function<E, U> function;
+  private Expression expression;
+  private Function function;
 
   public ExpressionAdaptor(Expression<T, E> pExpression, Function<E, U> pFunction) {
     expression = pExpression;
     function = pFunction;
   }
 
+  public ExpressionAdaptor(Expression<T, U> pExpression) {
+    expression = pExpression;
+  }
+
   @Override
   public U eval(T pT) {
-    E eval = expression.eval(pT);
+    Object eval = expression.eval(pT);
     if (eval == null) {
       return null;
     }
-    return function.apply(eval);
+
+    if (function == null) {
+      return (U) eval;
+    }
+
+    return (U) function.apply(eval);
   }
 
   @Override
   public T $getRoot() {
-    return expression.$getRoot();
+    return (T) expression.$getRoot();
   }
 }
