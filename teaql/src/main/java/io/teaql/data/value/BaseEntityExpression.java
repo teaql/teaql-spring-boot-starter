716d3942579ef0317995cf03ca02eace4b283e6d
jackytian <tianbook@126.com>
2023-05-16 11:27:47 +0800

    add save

--- teaql/src/main/java/io/teaql/data/value/BaseEntityExpression.java
+++ teaql/src/main/java/io/teaql/data/value/BaseEntityExpression.java
@@ -1,6 +1,7 @@
 package io.teaql.data.value;
 
 import io.teaql.data.BaseEntity;
+import io.teaql.data.UserContext;
 
 public interface BaseEntityExpression<T, U extends BaseEntity> extends Expression<T, U> {
   default Expression<T, Long> getId() {
@@ -9,6 +10,10 @@ public interface BaseEntityExpression<T, U extends BaseEntity> extends Expressio
 
   default Expression<T, Long> getVersion() {
     return apply(BaseEntity::getVersion);
+  }
+
+  default Expression<T, U> save(UserContext userContext) {
+    return apply(entity -> (U) entity.save(userContext));
   }
 
   default Expression<T, U> updateId(Long id) {
