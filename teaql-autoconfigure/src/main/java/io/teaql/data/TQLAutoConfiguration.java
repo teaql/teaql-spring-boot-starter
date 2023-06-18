03bc5b4b75cbe3e78d3633c19273f36228ea4abe
jackytian <tianbook@126.com>
2023-06-18 16:23:11 +0800

    错误修复

--- teaql-autoconfigure/src/main/java/io/teaql/data/TQLAutoConfiguration.java
+++ teaql-autoconfigure/src/main/java/io/teaql/data/TQLAutoConfiguration.java
@@ -1,33 +1,34 @@
 package io.teaql.data;
 
+import cn.hutool.core.util.ReflectUtil;
 import io.teaql.data.meta.EntityMetaFactory;
 import io.teaql.data.meta.SimpleEntityMetaFactory;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.reactive.BindingContext;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class TQLAutoConfiguration {
 
   @Bean
-  @ConditionalOnMissingBean
-  @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-  public TQLContextResolver userContextResolver() {
-    return new TQLContextResolver(dataConfig());
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
-  public TQLReactiveContextResolver reactiveUserContextResolver() {
-    return new TQLReactiveContextResolver(dataConfig());
+  @ConfigurationProperties(prefix = "teaql")
+  public DataConfigProperties dataConfig() {
+    return new DataConfigProperties();
   }
 
   @Bean
@@ -36,35 +37,76 @@ public class TQLAutoConfiguration {
     return new SimpleEntityMetaFactory();
   }
 
-  @Bean
+  @Configuration
   @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-  public WebMvcConfigurer webMvcConfigurer(List<HandlerMethodArgumentResolver> custom) {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.addAll(custom);
-      }
-    };
-  }
+  public static class TQLContextResolver implements HandlerMethodArgumentResolver {
+    private DataConfigProperties config;
 
-  @Bean
-  @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
-  public WebFluxConfigurer reactiveConfigure(
-      List<org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver> custom) {
-    return new WebFluxConfigurer() {
-      @Override
-      public void configureArgumentResolvers(ArgumentResolverConfigurer configure) {
-        for (org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver
-            handlerMethodArgumentResolver : custom) {
-          configure.addCustomResolver(handlerMethodArgumentResolver);
+    public TQLContextResolver(@Autowired DataConfigProperties config) {
+      this.config = config;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+      return parameter.hasParameterAnnotation(TQLContext.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+        MethodParameter parameter,
+        ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory)
+        throws Exception {
+      Class<? extends UserContext> contextType = config.getContextClass();
+      UserContext userContext = ReflectUtil.newInstanceIfPossible(contextType);
+      userContext.init(webRequest.getNativeRequest());
+      return userContext;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(TQLContextResolver tqlResolver) {
+      return new WebMvcConfigurer() {
+        @Override
+        public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+          resolvers.add(tqlResolver);
         }
-      }
-    };
+      };
+    }
   }
 
-  @Bean
-  @ConfigurationProperties(prefix = "teaql")
-  public DataConfigProperties dataConfig() {
-    return new DataConfigProperties();
+  @Configuration
+  @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+  public static class TQLReactiveContextResolver
+      implements org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver {
+    private DataConfigProperties config;
+
+    public TQLReactiveContextResolver(@Autowired DataConfigProperties config) {
+      this.config = config;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+      return parameter.hasParameterAnnotation(TQLContext.class);
+    }
+
+    @Override
+    public Mono<Object> resolveArgument(
+        MethodParameter parameter, BindingContext bindingContext, ServerWebExchange exchange) {
+      Class<? extends UserContext> contextType = config.getContextClass();
+      UserContext userContext = ReflectUtil.newInstanceIfPossible(contextType);
+      userContext.init(exchange);
+      return Mono.just(userContext);
+    }
+
+    @Bean
+    public WebFluxConfigurer webFluxConfigurer(TQLReactiveContextResolver resolver) {
+      return new WebFluxConfigurer() {
+        @Override
+        public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
+          configurer.addCustomResolver(resolver);
+        }
+      };
+    }
   }
 }
