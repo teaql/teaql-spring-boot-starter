2ee82f2f8f5ac7182c5f1f8745ea2f719a1b5aad
jackytian <tianbook@126.com>
2023-07-22 11:16:27 +0800

    add object location, data service

---
+++ teaql-mysql/src/main/java/io/teaql/data/mysql/io/teaql/data/mysql/MysqlRepository.java
@@ -0,0 +1,12 @@
+package io.teaql.data.mysql.io.teaql.data.mysql;
+
+import io.teaql.data.Entity;
+import io.teaql.data.meta.EntityDescriptor;
+import io.teaql.data.sql.SQLRepository;
+import javax.sql.DataSource;
+
+public class MysqlRepository<T extends Entity> extends SQLRepository<T> {
+  public MysqlRepository(EntityDescriptor entityDescriptor, DataSource dataSource) {
+    super(entityDescriptor, dataSource);
+  }
+}
