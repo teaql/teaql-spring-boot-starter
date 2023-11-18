df09929dc00432769da174fab8274228b6bacab8
Danny Xie <pigdanx@126.com>
2023-11-18 11:05:01 +0800

    add snowflake module'

---
+++ teaql-snowflake/src/main/java/io/teaql/data/snowflake/SnowflakeRepository.java
@@ -0,0 +1,26 @@
+package io.teaql.data.snowflake;
+
+import io.teaql.data.Entity;
+import io.teaql.data.meta.EntityDescriptor;
+import io.teaql.data.sql.SQLRepository;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class SnowflakeRepository<T extends Entity> extends SQLRepository<T> {
+  public SnowflakeRepository(EntityDescriptor entityDescriptor, DataSource dataSource) {
+    super(entityDescriptor, dataSource);
+  }
+
+  @Override
+  protected String findTableColumnsSql(DataSource dataSource, String table) {
+    try (Connection connection = dataSource.getConnection()) {
+      String databaseName = connection.getCatalog();
+      return String.format(
+          "select * from information_schema.columns where table_name = '%s' and table_schema = '%s'",
+          table, databaseName);
+    } catch (SQLException pE) {
+      throw new RuntimeException(pE);
+    }
+  }
+}
