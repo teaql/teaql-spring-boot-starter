98fd0dff0f4a4a1287e9fb33e19d9e69ff23cc08
Danny Xie <pigdanx@126.com>
2023-11-19 23:21:11 +0800

    snowflake 区分大小写

--- teaql-snowflake/src/main/java/io/teaql/data/snowflake/SnowflakeRepository.java
+++ teaql-snowflake/src/main/java/io/teaql/data/snowflake/SnowflakeRepository.java
@@ -1,10 +1,18 @@
 package io.teaql.data.snowflake;
 
+import cn.hutool.core.util.StrUtil;
 import io.teaql.data.Entity;
+import io.teaql.data.RepositoryException;
+import io.teaql.data.UserContext;
 import io.teaql.data.meta.EntityDescriptor;
+import io.teaql.data.sql.SQLColumn;
 import io.teaql.data.sql.SQLRepository;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
 
 public class SnowflakeRepository<T extends Entity> extends SQLRepository<T> {
@@ -23,5 +31,62 @@ public class SnowflakeRepository<T extends Entity> extends SQLRepository<T> {
     } catch (SQLException pE) {
       throw new RuntimeException(pE);
     }
+  }
+
+  @Override
+  protected String getPureColumnName(String columnName) {
+    return StrUtil.unWrap(columnName.toUpperCase(), '\"');
+  }
+
+  @Override
+  protected String calculateDBType(Map<String, Object> columnInfo) {
+    String dataType = ((String) columnInfo.get("data_type")).toLowerCase();
+    switch (dataType) {
+      case "bigint":
+        return "bigint";
+      case "tinyint":
+      case "boolean":
+        return "boolean";
+      case "varchar":
+      case "character varying":
+        return StrUtil.format("varchar({})", columnInfo.get("character_maximum_length"));
+      case "date":
+        return "date";
+      case "int":
+      case "integer":
+        return "integer";
+      case "number":
+        return "number";
+      case "decimal":
+      case "numeric":
+        return StrUtil.format(
+                "numeric({},{})", columnInfo.get("numeric_precision"), columnInfo.get("numeric_scale"));
+      case "text":
+        return "text";
+      case "time without time zone":
+        return "time";
+      case "timestamp":
+      case "timestamp_ntz":
+      case "timestamp without time zone":
+        return "timestamp";
+      default:
+        throw new RepositoryException("未处理的类型:" + dataType);
+    }
+  }
+
+  @Override
+  protected void ensure(
+          UserContext ctx, List<Map<String, Object>> tableInfo, String table, List<SQLColumn> columns) {
+    List<Map<String, Object>> upperCaseTableInfo = new ArrayList<>();
+    for (Map<String, Object> column : tableInfo) {
+      Map<String, Object> upperCase = new HashMap<>();
+      for (Map.Entry<String, Object> field: column.entrySet()) {
+        if (field.getValue() != null) {
+          upperCase.put(field.getKey().toUpperCase(), field.getValue().toString().toUpperCase());
+        }
+      }
+      upperCaseTableInfo.add(upperCase);
+    }
+    super.ensure(ctx, upperCaseTableInfo, table, columns);
   }
 }
