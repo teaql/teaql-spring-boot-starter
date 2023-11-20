844d3b8bc9145241b23110ffb7bcde3c16364f47
jackytian <tianbook@126.com>
2023-11-20 10:15:53 +0800

    enhance children update

--- teaql/src/main/java/io/teaql/data/repository/AbstractRepository.java
+++ teaql/src/main/java/io/teaql/data/repository/AbstractRepository.java
@@ -145,16 +145,11 @@ public abstract class AbstractRepository<T extends Entity> implements Repository
     for (T t : dataSet) {
       itemLocation.put(t.getId(), i++);
     }
-    Map<String, List<T>> itemsByType = dataSet.groupBy(item -> item.runtimeType());
     childrenRequest.forEach(
         (type, childRequest) -> {
-          List<T> subItems = itemsByType.get(type);
-          if (ObjectUtil.isEmpty(subItems)) {
-            return;
-          }
           TempRequest tempRequest = new TempRequest(childRequest);
           tempRequest.appendSearchCriteria(
-              tempRequest.createBasicSearchCriteria(BaseEntity.ID_PROPERTY, Operator.IN, subItems));
+              tempRequest.createBasicSearchCriteria(BaseEntity.ID_PROPERTY, Operator.IN, dataSet));
           SmartList childrenItems = tempRequest.executeForList(userContext);
           for (Object item : childrenItems) {
             T subItem = (T) item;
