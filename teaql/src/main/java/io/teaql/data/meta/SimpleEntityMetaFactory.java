05332c58dbd0a60895cb0b486936b7ccfadea634
Philip Zhang <zhangxilai@doublechaintech.com>
2023-12-13 08:13:06 +0800

    🚀fix spell

--- teaql/src/main/java/io/teaql/data/meta/SimpleEntityMetaFactory.java
+++ teaql/src/main/java/io/teaql/data/meta/SimpleEntityMetaFactory.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Component
+
 
 public class SimpleEntityMetaFactory implements EntityMetaFactory {
   Map<String, EntityDescriptor> registeredEntities = new ConcurrentHashMap<>();
